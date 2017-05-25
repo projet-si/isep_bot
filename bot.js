8d15ed3f548e7bfab8c0e28fa535a85f622bb905
Maxime Pasqualini <maximepasqualini@hotmail.fr>
2017-05-25 22:13:37 +0200

    Implémentation du service youtube

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+var YouTube = require('youtube-node')
+var youtube = new YouTube()
+youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
-
-client.on('message', msg => {
+  client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
   if (msg.channel.type !== 'dm' && (!config.channels[msg.channel.id] || msg.author.id === client.user.id)) return
@@ -15,6 +17,19 @@ client.on('message', msg => {
   if (msg.content === 'hello') {
     msg.channel.sendMessage('Hello to you too, fellow !')
   }
-})
-
+  if (msg.content.startsWith("!youtube ")) {
+    youtube.search(msg.content.substring(6), 3, function(error, result) {
+      if (error) {
+        console.log(error)
+      }
+      else {
+        //console.log(JSON.stringify(result, null, 2))
+        var stockitemty = result.items;
+        msg.channel.sendMessage('Voilà le premier lien en rapport avec votre recherche https://www.youtube.com/watch?v=' + stockitemty[0].id.videoId)
+        msg.channel.sendMessage('Et voici le deuxième https://www.youtube.com/watch?v=' + stockitemty[1].id.videoId)
+        msg.channel.sendMessage('Et enfin le dernier https://www.youtube.com/watch?v=' + stockitemty[2].id.videoId)
+      }
+    })
+  }
+  })
 client.login(config.token)
