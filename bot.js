bcd55286f559545a2c0c74316ea0ce4a321653e5
Maxime Pasqualini <maximepasqualini@hotmail.fr>
2017-05-25 22:13:37 +0200

    Implémentation du service youtube

--- bot.js
+++ bot.js
@@ -8,12 +8,14 @@ var spotifyApi = new SpotifyWebApi({
   clientId: '533a5af89e504b7393f75c477f13c3cd',
   clientSecret: '09e925930f1547b8b95d7696272e3c32'
 })
+var YouTube = require('youtube-node')
+var youtube = new YouTube()
+youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
-
-client.on('message', msg => {
+  client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
@@ -99,6 +101,19 @@ client.on('message', msg => {
   console.log('Something went wrong!', err)
 })
   }
-})
-
+  if (msg.content.startsWith("!youtube ")) {
+    youtube.search(msg.content.substring(6), 3, function(error, result) {
+      if (error) {
+        console.log(error)
+      }
+      else {
+        //console.log(JSON.stringify(result, null, 2))
+        var stockitemty = result.items;
+        msg.channel.sendMessage('Voilà le premier lien en rapport avec votre recherche https://www.youtube.com/watch?v=' + stockitemty[0].id.videoId)
+        msg.channel.sendMessage('Et voici le deuxième https://www.youtube.com/watch?v=' + stockitemty[1].id.videoId)
+        msg.channel.sendMessage('Et enfin le dernier https://www.youtube.com/watch?v=' + stockitemty[2].id.videoId)
+      }
+    })
+  }
+  })
 client.login(config.token)
