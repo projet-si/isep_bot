36af9d2b2a9c8c09346fb0aca599b54572af9f0c
Maxime Pasqualini <maximepasqualini@hotmail.fr>
2017-05-26 08:57:47 +0200

    first part of youtube

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const config = require('./config.js')
 const client = new Discord.Client()
 var YouTube = require('youtube-node')
 var youtube = new YouTube()
-youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU')
+youtube.setKey('AIzaSyA961WArdG_rzAFtRqEaWsE98ZiM4Sa7Lw')
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -16,6 +17,9 @@ client.on('ready', () => {
   // If message is hello, post hello too
   if (msg.content === 'hello') {
     msg.channel.sendMessage('Hello to you too, fellow !')
+  }
+  if (msg.content === '!help') {
+    msg.channel.sendMessage('Hi you can use the youtube service by typing "!youtube myresearch"! I will do the best for you')
   }
   if (msg.content.startsWith("!youtube ")) {
     youtube.search(msg.content.substring(6), 3, function(error, result) {
@@ -31,5 +35,5 @@ client.on('ready', () => {
       }
     })
   }
-  })
+})
 client.login(config.token)
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEyNTAyNjkyODY4MjU5ODQx.DAhlKg._BM2xd_nDI10-Pbv8_iqUnYPR-0',
   channel: 'channelid'
 }
